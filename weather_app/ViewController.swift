b249fc77fc1d18fba6a8063d5c442511352bd863
Tony Wang <tiancheng_wang@brown.edu>
2021-04-23 21:45:35 -0400

    Initial Commit

---
+++ weather_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weather_app
+//
+//  Created by Tony Wang on 4/23/21.
+//  Copyright © 2021 Tony_Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ weather_appTests/weather_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  weather_appTests.swift
+//  weather_appTests
+//
+//  Created by Tony Wang on 4/23/21.
+//  Copyright © 2021 Tony_Wang. All rights reserved.
+//
+
+import XCTest
+@testable import weather_app
+
+class weather_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
